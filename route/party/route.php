87aabcd54f3cf1ca8c570ba60e1f0fcc9e8cdc14
CharlesChanV <charleschan2015@hotmail.com>
2020-04-18 16:02:04 +0800

    1完善用户控制器

--- route/party/route.php
+++ route/party/route.php
@@ -6,3 +6,6 @@ Route::post(':version/admin/register',':version.Admin/adminRegister');
 Route::get(':version/test', ':version.Admin/test');
 Route::get(':version/user', ':version.User/index');
 Route::get(':version/user/:id', ':version.User/read');
+Route::post(':version/user', ':version.User/save');
+Route::put(':version/user/:id', ':version.User/update');
+Route::delete(':version/user/:id', ':version.User/delete');
