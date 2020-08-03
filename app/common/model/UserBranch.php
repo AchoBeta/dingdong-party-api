60053f91f3728b8944addabe3cd38d43272193d7
hua-bang <1964760211@qq.com>
2020-08-03 19:41:30 +0800

    修改文件存放路径 修改入党联系人字段 修改临时的常量 修改用户与联系人的关联关系

--- app/common/model/UserBranch.php
+++ app/common/model/UserBranch.php
@@ -13,16 +13,16 @@ class UserBranch extends Model
     protected $autoWriteTimestamp = true;
     protected $json = ['investigation_in_half_year_activist','opinions_for_supply_activist','opinions_for_supply_prepare','application_info'];
     public function firstContact(){
-        return $this->belongsTo(Contacts::class,'develop_contact_first');
+        return $this->belongsTo(Contacts::class,'develop_contact_first_id');
     }
     public function secondContact(){
-        return $this->belongsTo(Contacts::class,'develop_contact_second');
+        return $this->belongsTo(Contacts::class,'develop_contact_second_id');
     }
 
     public function firstRecommendContact(){
-        return $this->hasMany(Contacts::class,'id',"recommend_first");
+        return $this->hasOne(Contacts::class,'id',"recommend_first");
     }
     public function secondRecommendContact(){
-        return $this->hasMany(Contacts::class,'id',"recommend_first");
+        return $this->hasOne(Contacts::class,'id',"recommend_first");
     }
 }
